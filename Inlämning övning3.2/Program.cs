a6e764b859b7cc182a820f16d53ad7f0f06bb572
ahka07001 <ahka07001@E-PW05CGQT>
2024-11-14 15:19:47 +0100

    Add project files.

---
+++ Inlämning övning3.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace inlämning_._övning_._3_._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått i gymnasiet? (Skriv j för ja och n för nej)");
+            string skola= Console.ReadLine();
+
+            Console.WriteLine("Hur gammal är du?");
+            int år=int.Parse(Console.ReadLine());
+
+            if (skola == "j" && år < 22)
+                Console.WriteLine("Vi vill gärna anställa dig");
+            else
+                Console.WriteLine("Vi letar tyvärr efter annan personal just nu");
+            
+        }
+    }
+}
